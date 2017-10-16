391337b22bf7d2d158eeed1352e1135671e772a0
Jonathan <sifreed77@gmail.com>
2017-10-15 21:21:00 -0400

    Added gnome sort php implementation

---
+++ algorithms/gnomeSortImplementation.php
@@ -0,0 +1,16 @@
+public static function gnomeSort(array $array){
+	$i = 1;
+	while($i < count($array)){
+		if($array[$i] >= $array[$i - 1]){
+			$i++;
+		}else{
+			$tmp = $array[$i];
+			$array[$i] = $array[$i - 1];
+			$array[$i - 1] = $tmp;
+			if($i > 1){
+				$i--;
+			}
+		}
+	}
+	return $array;
+}
