0b024922424510ff5ab43b1b9f79f1d56b499b29
Jonathan <sifreed77@gmail.com>
2017-10-15 21:20:32 -0400

    Added comb sort implementation

---
+++ algorithms/combSortImplementation.php
@@ -0,0 +1,25 @@
+public static function combSort(array $array){
+	$swapped = false;
+	$j = 0;
+	$length = count($array);
+	$gap = $length;
+	
+	while($gap > 1 || $swapped){
+		if($gap > 1){
+			$gap = floor($gap/1.2473);
+		}
+		
+		$swapped = false;
+		$j++;
+		
+		for($i=0; $i+$gap < $length; i++){
+			if($array[$i] > $array[$i + $gap]){
+				$tmp = $array[$i];
+				$array[$i] = $array[$i + $gap];
+				$array[$i + $gap] = $tmp;
+				$swapped = true;
+			}
+		}
+	}
+	return $array;
+}
