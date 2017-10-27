6a024c67c35ee00b4e00cb7085728160b4d40084
Jonathan <sifreed77@gmail.com>
2017-10-26 22:57:57 -0400

    Added checkSort implementation

---
+++ algorithms/checkSortImplementation.php
@@ -0,0 +1,14 @@
+function checkSort($array){
+    if(!$length = count($array)){
+        return truel;
+    }
+
+    for($i = 0; $i < $length; $i++){
+        if(isset($array[$i + 1])){
+            if($array[$i] > $array[$i + 1]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
