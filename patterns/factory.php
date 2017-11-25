802526e96f8f3c9288d1c732841991c339d19548
Jonathan <sifreed77@gmail.com>
2017-11-24 22:11:30 -0500

    Added factory pattern implementation

---
+++ patterns/factory.php
@@ -0,0 +1,23 @@
+<?php
+interface IUser{
+	function getName();
+}
+
+class User implements IUser{
+	public function __construct($id){}
+	
+	public function getName(){
+		return "Mark";
+	}
+}
+
+class UserFactory{
+	public static function Create($id){
+		return new User($id);
+	}
+}
+
+$uo = UserFactory::Create(1);
+echo($uo->getName()."\n");
+
+?>
