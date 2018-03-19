973c8ac47221dfbf717654ca6759a5ba98d1bdbe
Jonathan <sifreed77@gmail.com>
2018-03-18 23:45:33 -0400

    Updated index.php

--- cake/app2/webroot/index.php
+++ cake/app2/webroot/index.php
@@ -36,5 +36,6 @@ use Cake\Http\Server;
 // Bind your application to the server.
 $server = new Server(new Application(dirname(__DIR__) . '/config'));
 
+
 // Run the request/response through the application and emit the response.
 $server->emit($server->run());
