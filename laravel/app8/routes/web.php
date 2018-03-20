bfa9697f31a0ae5192d6df4f896f433b0186edec
Jonathan <sifreed77@gmail.com>
2018-03-19 23:19:49 -0400

    Added app8 implementation

---
+++ laravel/app8/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('employee', 'EmployeeController@create')->name('employee.create');
+Route::post('employee', 'EmployeeController@store')->name('employee.store');
