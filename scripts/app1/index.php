e9383eb64fd96dd5485e2ef39aa295b56a5b297f
Jonathan <sifreed77@gmail.com>
2018-03-22 23:45:15 -0400

    Updated index.php

--- scripts/app1/index.php
+++ scripts/app1/index.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Contact Form by Fullarrays</title>
+        <title>Contact Form by Fullarray</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -13,11 +13,12 @@
     </head>
     <body>
         <!-- added for IE supporr  -->
+
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
         <!-- code starts here  -->
-        <h2>Contact me</h2>
+        <h2>Contact me (FullArray)</h2>
         <form name="sendmail" action="" method="post">
             <label>Name</label>
             <input class="pretty" name="fname" type="text"/>
