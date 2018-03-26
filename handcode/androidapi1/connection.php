f6625b21c5e71c30ddc0cbeb46e0550b80ab392d
Jonathan <sifreed77@gmail.com>
2018-03-25 22:07:17 -0400

    Added androidReg implementation

---
+++ handcode/androidapi1/connection.php
@@ -0,0 +1,15 @@
+<?php
+	require_once 'config.php';
+	
+	class DB_Connection{
+		private $connect;
+		function __construct(){
+			$this->connect = mysqli_connect(hostname, user, password, db_name)
+			or die("Could not connect to db.");
+		}
+		
+		function getConnection(){
+			return $this->connect;
+		}
+	}
+?>
