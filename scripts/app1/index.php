0e80de33dd339fbbd0f5223791d843c384eeb69c
Jonathan <sifreed77@gmail.com>
2018-04-22 23:33:08 -0400

    Update index.php

--- scripts/app1/index.php
+++ scripts/app1/index.php
@@ -13,7 +13,6 @@
     </head>
     <body>
         <!-- added for IE supporr  -->
-
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
