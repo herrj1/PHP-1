941befbee3107d756769843bda37c843b946f812
Jonathan <sifreed77@gmail.com>
2018-05-18 22:31:11 -0400

    Added app20 implementation

---
+++ laravel/app20/app/Models/Film.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Film extends Model
+{
+	//
+	use Searchable;
+	
+    //
+	protected $primaryKey = 'film_id';
+	
+	//
+	protected $table = 'film';
+	
+	//
+	public function searchableAs(){
+		return 'films_index';
+	}
+}
---
+++ laravel/app20/bootstrap/cache/packages.php
@@ -0,0 +1,37 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/scout' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Scout\\ScoutServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'teamtnt/laravel-scout-tntsearch-driver' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'TeamTNT\\Scout\\TNTSearchScoutServiceProvider',
+    ),
+  ),
+);
