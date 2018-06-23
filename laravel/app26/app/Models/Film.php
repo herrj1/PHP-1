a84c19d952829a54397f0179c1b439eefb513a11
Jonathan <sifreed77@gmail.com>
2018-06-22 20:51:34 -0400

    Added config implementation

---
+++ laravel/app26/app/Models/Film.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+	use Searchable;
+    
+	//
+	protected $primaryKey = 'film_id';
+	
+	//
+	protected $table = 'film';
+	
+	
+	public function searchableAs(){
+		return 'films_index';
+	}
+}
