331a468f635000eaaef0166bc5bcd806b56665bb
Jonathan <sifreed77@gmail.com>
2018-07-16 23:22:15 -0400

    Added calculussummationwithsigmanotation implementation

---
+++ handcode/calculussummationwithsigmanotation.php
@@ -0,0 +1,11 @@
+function sumfunction($start, $length){
+ $sum = 0;
+ for($i = $start; $i <= $length ; $i++){$sum += 1/(pow($i, 2)+9);}
+ return $sum .' ';
+}
+
+//Sample run
+echo sumfunction(2,6);
+
+//Sample output
+0.22411261940674
