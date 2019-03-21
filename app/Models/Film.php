6c0b94e25cba68cef12d97a9ddb64f0aeaa21ce8
Jonathan <sifreed77@gmail.com>
2019-03-20 23:21:43 -0400

    Update Film.php

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -8,13 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Film extends Model
 {
 	use Searchable;
-    
 	//
 	protected $primaryKey = 'film_id';
 	
 	//
 	protected $table = 'film';
-	
 	
 	public function searchableAs(){
 		return 'films_index';
