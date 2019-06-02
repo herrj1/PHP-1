ed779afe4ef286da9e8d185acff0326041119ecf
Jonathan <sifreed77@gmail.com>
2019-06-01 23:57:33 -0400

    Update index.php

--- lumen/app2/public/index.php
+++ lumen/app2/public/index.php
@@ -15,7 +15,6 @@ $app = require __DIR__.'/../bootstrap/app.php';
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
-|
 | Once we have the application, we can handle the incoming request
 | through the kernel, and send the associated response back to
 | the client's browser allowing them to enjoy the creative
