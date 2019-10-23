db7232296383af7f84ba5e553bd55ebf86efb139
Jonathan <sifreed77@gmail.com>
2019-10-22 22:46:59 -0400

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,9 @@
 
 use Faker\Generator as Faker;
 /*
-|--------------------------------------------------------------------------
+|------------------------------------------------------------------------
 | Model Factories
-|--------------------------------------------------------------------------
+|------------------------------------------------------------------------
 |
 | This directory should contain each of the model factory definitions for
 | your application. Factories provide a convenient way to generate new
