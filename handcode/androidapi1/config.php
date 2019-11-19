edb68a8a6fe4b61e54632272239d422e29b106a1
Jonathan <sifreed77@gmail.com>
2019-11-18 23:47:54 -0500

    Update config.php

--- handcode/androidapi1/config.php
+++ handcode/androidapi1/config.php
@@ -1,6 +1,11 @@
 <?php
 	define('hostname', 'localhost');
 	define('user','root');
-	define('password','');
-	define('db_name', 'androidapi1');
+
+define('password','');
+
+
+define('db_name', 'androidapi1');
+
+
 ?>
