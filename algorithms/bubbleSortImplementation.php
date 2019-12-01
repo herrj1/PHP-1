7116b3f007c5b82fbf3716a1aaf577b32fae2914
Jonathan <sifreed77@gmail.com>
2019-11-30 23:31:22 -0500

    Update bubbleSortImplementation.php

--- algorithms/bubbleSortImplementation.php
+++ algorithms/bubbleSortImplementation.php
@@ -2,8 +2,7 @@ function bubbleSort($array){
 	if(!$length = count($array)){
 		return $array;
 	}
-
-        //Loop through every element(s)
+        //Loops through every element(s)
 	for($outer=0; $outer < $length; $outer++){
 		for($inner=0; $inner < $length; $inner++){
 			if($array[$outer] < $array[$inner]){
