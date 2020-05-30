f2f9f389de69000acc9a31e96dd52cf62551ba90
Jonathan <sifreed77@gmail.com>
2020-05-29 23:00:11 -0400

    Update cache.php

--- config/cache.php
+++ config/cache.php
@@ -3,16 +3,15 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
+    |--------------------------------------------------------------------
     | Default Cache Store
-    |--------------------------------------------------------------------------
+    |--------------------------------------------------------------------
     |
     | This option controls the default cache connection that gets used while
     | using this caching library. This connection is used when another is
     | not explicitly specified when executing a given caching function.
     |
     | Supported: "apc", "array", "database", "file", "memcached", "redis"
-    |
     */
     'default' => env('CACHE_DRIVER', 'file'),
     /*
