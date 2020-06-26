82054e79acfec6eb1d518d4b7ae647ba78c0db07
Jonathan <sifreed77@gmail.com>
2020-06-25 23:10:37 -0400

    Update packages.php

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,7 +1,6 @@
 <?php return array (
   'fideloper/proxy' => 
-  array (
-    'providers' => 
+  array ('providers' => 
     array (
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
