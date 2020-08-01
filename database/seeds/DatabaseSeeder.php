054182f7e508ae786f88b0b1977539c4f9b9426e
Jonathan <sifreed77@gmail.com>
2020-07-31 23:22:15 -0400

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,10 +1,9 @@
 <?php
-
 use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seeds the application's database.
      * @return void
      */
     public function run()
